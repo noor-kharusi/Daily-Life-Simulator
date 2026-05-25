65d1b3ca4dc22b2c95dfdeac4430521e2112eac4
1u6n4 <lubnamhmd97@gmail.com>
2026-05-25 18:56:49 +0400

    Add files via upload

--- activity_class.cpp
+++ activity_class.cpp
@@ -1,3 +1,7 @@
+//
+// Created by lubna on 5/12/2026.
+//
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,11 +11,15 @@ protected:
     string name;
 
 public:
+    // constructor
     activity(string n) {
         name = n;
     }
 
-    virtual void apply(int &energy, int &mood, int &productivity) = 0;
+    virtual void apply(int &energy, int &mood,
+                       int &productivity,
+                       int &stress,
+                       int &health) = 0;
 
     void showname() {
         cout << "activity: " << name << endl;
@@ -23,28 +31,41 @@ public:
     study() : activity("study") {
     }
 
-    void apply(int &energy, int &mood, int &productivity) {
+    void apply(int &energy, int &mood,
+               int &productivity,
+               int &stress,
+               int &health) {
+
         cout << "you studied today" << endl;
+
         productivity += 5;
+        stress += 7;
         energy -= 10;
+        mood -= 2;
     }
 };
 
 int main() {
+
     int energy = 100;
     int mood = 100;
     int productivity = 100;
+    int stress = 0;
+    int health = 100;
 
     study s;
 
     s.showname();
 
-    s.apply(energy, mood, productivity);
+    s.apply(energy, mood, productivity, stress, health);
 
     cout << endl;
+
     cout << "energy: " << energy << endl;
     cout << "mood: " << mood << endl;
     cout << "productivity: " << productivity << endl;
+    cout << "stress: " << stress << endl;
+    cout << "health: " << health << endl;
 
     return 0;
 }
